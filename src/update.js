963b625b619632230b0ee48558dcf722b1080397
randlos <mr.teddyfocus@gmail.com>
2019-08-26 16:09:05 +0200

    Multiple Barcharts now working again

--- src/update.js
+++ src/update.js
@@ -179,6 +179,7 @@ function update() {
 
                 }
                 //console.log(columnlist);
+                //console.log("Type Array" + typeof(columnlist));
                 return columnlist;
             }
 
@@ -195,7 +196,7 @@ function update() {
                         };
                     }
                 }
-                //console.log("Column Array: " + "["+columnArray+"]");
+                //console.log("Column Array: " + "[" + columnArray + "]");
                 return columnArray;
             } else {
                 let i;
@@ -204,7 +205,7 @@ function update() {
                     if (alphaList[i].string == alpha) {
                         //console.log("Output:" + alphaList[i].number);
                         barchart_column = alphaList[i].number - 1;
-
+                        //console.log(barchart_column);
                         return barchart_column;
                     };
                 };
@@ -242,20 +243,31 @@ function update() {
     }
 
     function without_bar(number, data) {
-        //console.log("inputnumber: " + number);
+
         let value_array = data.Data[0].values;
+
         let column_array = new Array();
         for (var index in value_array) {
+            if (isNaN(number)) {
+                for (var j in number) {
+                    if (index != j && index != 0) {
+                        console.log("Pop Index: " + j);
+                        column_array.pop(parseInt(index));
+                    }
+                }
+            }
             if (index != number && index != 0) {
+                console.log("Push Index: " + index)
                 column_array.push(parseInt(index));
             }
         }
+        console.log(column_array);
         return column_array;
     }
 
     function number_format(data) {
 
-        if (Math.abs(data) > 10000 && Math.abs(data) < 1000000) {
+        if (Math.abs(data) >= 10000 && Math.abs(data) <= 1000000) {
             //console.log(thousand(data));
             return thousand(data);
         } else if (Math.abs(data) >= 1000000) {
@@ -401,130 +413,132 @@ function update() {
         //responsive: true,
         //"autoWidth": true,
         "columnDefs": [{
-            "targets": 0,
-            "data": 0,
-            "render": function(data, type, row, meta) {
-                if (data.indexOf("https://") > -1) {
-                    var img_tag = '<img src="' + data + '"height="' + state.imgsize_h + '"width="' + state.imgsize_w + '">';
-                    //console.log("height: " + state.imgsize[0] + ", width: " + state.imgsize[1]);
-                    return img_tag;
-                } else {
-                    return data;
-                }
-            },
-        }, {
-            "targets": without_bar(tranlsateSortingAlphaToNumber(state.bar_column), data),
-            "render": function(data, type, row, meta) {
-                if (type == "display") {
-                    return number_format(data);
-                }
-                return getjustnumber(data);
-            }
-        }, {
-            "targets": tranlsateSortingAlphaToNumber(state.bar_column),
-            "render": function(data, type, row, meta) {
-
-                let maxVal = maxValue(meta.col);
-                let minVal = minValue(meta.col);
-                //console.log("MinValue: " + minVal);
-                let rangeMax = maxVal - minVal;
-                //console.log("rangeMax: " + rangeMax);
-
-                // Adjust the max to 100% and distribute to min
-                let maxNormalize = (Math.abs(getjustnumber(data)) / maxVal) * 100;
-                // ((data - minVal+1)/rangeMax) * 100 --> Get the difference between the actual data-value and the range to map the data from minValue = 1 (+1) to maxValue = 100 (+1)
-                let minMaxNormalize_plus = ((Math.abs(getjustnumber(data)) - minVal) / rangeMax) * 90;
-                let minMaxNoralize_minus = ((Math.abs(getjustnumber(data)) - minVal) / rangeMax) * 53;
-
-
-                // console.log("input: " + Math.abs(getjustnumber(data)));
-                //console.log("minVal: " + minVal);
-                //console.log("output: " + (Math.abs(getjustnumber(data)) - Math.abs(minVal)));
-                // console.log("____");
-                // console.log("minMaxNormalized: " + minMaxNormalize_plus);
-                // console.log("rangeMax: " + rangeMax);
-                // console.log("____");
-
-
-                // console.log("maxValue: " + maxVal);
-                // console.log("minValue: " + minVal);
-                // console.log("minMaxNoralize: " + minMaxNormalize);
-
-                //let bar_data = Math.abs(getjustnumber(data));
-
-                //getjuststring(data);
-
-
-                //console.log("Max Value in function: " + maxVal);
-                if (type == "display") {
-                    if (state.bar_switch || state.bar_column > 0) { //
-
-
-                        if (isNaN(getjustnumber(data))) {
-                            //console.log("data is not a number");
-                            return data;
-
-                        } else if (state.negative_bar) {
-                            let pre_bar_container = '<div class="barcont">';
-
-                            let lefttd_start = '<div class="leftbar">';
-                            let lefttd_end = '</div>';
-                            let righttd_start = '<div class="rightbar">';
-                            let righttd_end = '</div>';
-
-
-
-                            let left_content = '<p style="text-align:right;margin:0 4px 0 0;">' + number_format(data) + '</p>';
-                            let right_content = '<p style="text-align:left;margin:0 0 0 4px;">' + number_format(data) + '</p>';
-
-                            // 
-
-                            let right_bar = '<div class="bardiv"> <span class="bar" style="height:19px;margin: 3px 0 0 0;width:' + minMaxNoralize_minus + '%; background: ' + state.color_balken_positive + '"></span></div>';
-                            let left_bar = '<div class="bardiv"> <span class="bar" style="float:right;margin:0;height:20px;margin: 3px 1px 0 0;width:' + minMaxNoralize_minus + '%; background:' + state.color_balken_negative + '"></span></div>';
-
-                            let post_bar_container = '</div>';
-
-
-                            let zerovalue = pre_bar_container + '<p style="text-align: center; margin:0;">' + data + '</p>' + post_bar_container;
-                            let positive = pre_bar_container + lefttd_start + left_content + lefttd_end + righttd_start + right_bar + righttd_end + post_bar_container;
-                            let negative = pre_bar_container + lefttd_start + left_bar + lefttd_end + righttd_start + right_content + righttd_end + post_bar_container;
-
-                            if (getjustnumber(data) < 0) {
-                                //console.log(Math.abs(getjustnumber(data)));  
-                                return negative;
-                            } else if (getjustnumber(data) == 0) {
-                                return zerovalue;
-                            } else {
-                                return positive
-                            }
-
-
-                        } else {
-                            let pre_bar_container = '<div class="barcont">';
-                            let bartext = '<div class="bartext"><p style="color:#000000">' + data + '</p></div>';
-                            // if (getjustnumber(data) < 0) {
-                            //     let bar = '<div class="bardiv"> <span class="bar" style="height:20px;width:' + rangeMax + '%;background: #DD0000"></span></div>';
-                            // }
-                            //console.log("Test minMaxNormalize" + minMaxNormalize);
-
-                            // BARCHART WITH DIV
-                            let bar = '<div class="bardiv"> <span class="bar" style="lheight:20px;width:' + minMaxNormalize_plus + '%; background:' + state.color_balken_positive + '"></span></div>';
-                            // colorMapBalken(data, minVal, maxVal)  / console.log(maxNormalize);
-                            let post_bar_container = '</div>';
-
-                            return pre_bar_container + bar + bartext + post_bar_container;
-                        }
+                "targets": 0,
+                "data": 0,
+                "render": function(data, type, row, meta) {
+                    if (data.indexOf("https://") > -1) {
+                        var img_tag = '<img src="' + data + '"height="' + state.imgsize_h + '"width="' + state.imgsize_w + '">';
+                        //console.log("height: " + state.imgsize[0] + ", width: " + state.imgsize[1]);
+                        return img_tag;
                     } else {
+                        return data;
+                    }
+                },
+            },
+            {
+                "targets": without_bar(tranlsateSortingAlphaToNumber(state.bar_column), data),
+                "render": function(data, type, row, meta) {
+                    if (type == "display") {
                         return number_format(data);
                     }
+                    return getjustnumber(data);
+                }
+            },
+            {
+                "targets": tranlsateSortingAlphaToNumber(state.bar_column),
+                "render": function(data, type, row, meta) {
+                    //console.log(state.bar_column);
+                    //console.log(tranlsateSortingAlphaToNumber(state.bar_column));
+                    let maxVal = maxValue(meta.col);
+                    let minVal = minValue(meta.col);
+                    //console.log("MinValue: " + minVal);
+                    let rangeMax = maxVal - minVal;
+                    //console.log("rangeMax: " + rangeMax);
+
+                    // Adjust the max to 100% and distribute to min
+                    let maxNormalize = (Math.abs(getjustnumber(data)) / maxVal) * 100;
+                    // ((data - minVal+1)/rangeMax) * 100 --> Get the difference between the actual data-value and the range to map the data from minValue = 1 (+1) to maxValue = 100 (+1)
+                    let minMaxNormalize_plus = ((Math.abs(getjustnumber(data)) - minVal) / rangeMax) * 90;
+                    let minMaxNoralize_minus = ((Math.abs(getjustnumber(data)) - minVal) / rangeMax) * 53;
+
+
+                    // console.log("input: " + Math.abs(getjustnumber(data)));
+                    //console.log("minVal: " + minVal);
+                    //console.log("output: " + (Math.abs(getjustnumber(data)) - Math.abs(minVal)));
+                    // console.log("____");
+                    // console.log("minMaxNormalized: " + minMaxNormalize_plus);
+                    // console.log("rangeMax: " + rangeMax);
+                    // console.log("____");
+
+
+                    // console.log("maxValue: " + maxVal);
+                    // console.log("minValue: " + minVal);
+                    // console.log("minMaxNoralize: " + minMaxNormalize);
+
+                    //let bar_data = Math.abs(getjustnumber(data));
+
+                    //getjuststring(data);
+
+
+                    //console.log("Max Value in function: " + maxVal);
+                    if (type == "display") {
+                        if (state.bar_switch) { //
+                            if (isNaN(getjustnumber(data))) {
+                                //console.log("data is not a number");
+                                return data;
+
+                            } else if (state.negative_bar) {
+                                let pre_bar_container = '<div class="barcont">';
+
+                                let lefttd_start = '<div class="leftbar">';
+                                let lefttd_end = '</div>';
+                                let righttd_start = '<div class="rightbar">';
+                                let righttd_end = '</div>';
+
+
+
+                                let left_content = '<p style="text-align:right;margin:0 4px 0 0;">' + number_format(data) + '</p>';
+                                let right_content = '<p style="text-align:left;margin:0 0 0 4px;">' + number_format(data) + '</p>';
+
+                                // 
+
+                                let right_bar = '<div class="bardiv"> <span class="bar" style="height:19px;margin: 3px 0 0 0;width:' + minMaxNoralize_minus + '%; background: ' + state.color_balken_positive + '"></span></div>';
+                                let left_bar = '<div class="bardiv"> <span class="bar" style="float:right;margin:0;height:20px;margin: 3px 1px 0 0;width:' + minMaxNoralize_minus + '%; background:' + state.color_balken_negative + '"></span></div>';
+
+                                let post_bar_container = '</div>';
+
+
+                                let zerovalue = pre_bar_container + '<p style="text-align: center; margin:0;">' + data + '</p>' + post_bar_container;
+                                let positive = pre_bar_container + lefttd_start + left_content + lefttd_end + righttd_start + right_bar + righttd_end + post_bar_container;
+                                let negative = pre_bar_container + lefttd_start + left_bar + lefttd_end + righttd_start + right_content + righttd_end + post_bar_container;
+
+                                if (getjustnumber(data) < 0) {
+                                    //console.log(Math.abs(getjustnumber(data)));  
+                                    return negative;
+                                } else if (getjustnumber(data) == 0) {
+                                    return zerovalue;
+                                } else {
+                                    return positive
+                                }
+
+
+                            } else {
+                                let pre_bar_container = '<div class="barcont">';
+                                let bartext = '<div class="bartext"><p style="color:#000000">' + data + '</p></div>';
+                                // if (getjustnumber(data) < 0) {
+                                //     let bar = '<div class="bardiv"> <span class="bar" style="height:20px;width:' + rangeMax + '%;background: #DD0000"></span></div>';
+                                // }
+                                //console.log("Test minMaxNormalize" + minMaxNormalize);
+
+                                // BARCHART WITH DIV
+                                let bar = '<div class="bardiv"> <span class="bar" style="lheight:20px;width:' + minMaxNormalize_plus + '%; background:' + state.color_balken_positive + '"></span></div>';
+                                // colorMapBalken(data, minVal, maxVal)  / console.log(maxNormalize);
+                                let post_bar_container = '</div>';
+
+                                return pre_bar_container + bar + bartext + post_bar_container;
+                            }
+                        } else {
+                            return number_format(data);
+                        }
+                    }
+
+                    return getjustnumber(data);
+
+
                 }
 
-                return getjustnumber(data);
-
-
             }
-
-        }],
+        ],
         "paging": false,
         "scrollY": rm_zeile_height(state.yscroll),
         //"scrollCollapse": true,
